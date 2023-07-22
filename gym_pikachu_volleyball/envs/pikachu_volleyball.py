561d316dce61e8be5f5ca3d292e4cc846d7b21ac
hank <hankluo6@gmail.com>
2023-07-22 11:59:25 +0800

    Remove multi environement

--- gym_pikachu_volleyball/envs/pikachu_volleyball.py
+++ gym_pikachu_volleyball/envs/pikachu_volleyball.py
@@ -34,28 +34,3 @@ class PikachuVolleyballEnv(gym.Env):
 
     def close(self) -> None:
         self.engine.close()
-
-class PikachuVolleyballMultiEnv(PikachuVolleyballEnv):
-    def __init__(self, render_mode: str):
-        super(PikachuVolleyballMultiEnv, self).__init__(False, False, render_mode)
-
-        self.action_space = spaces.MultiDiscrete([18, 18])
-
-    def step(self, actions: Tuple[int, int]) -> Tuple[np.ndarray, float, bool, bool, dict]:
-        converted_action = tuple(convert_to_user_input(actions[i], i) for i in range(2))
-        is_ball_touching_ground = self.engine.step(converted_action)
-        self.engine.viewer.update()
-        self._renderer.render_step()
-        if is_ball_touching_ground:
-            reward = 1 if self.engine.ball.punch_effect_x < GROUND_HALF_WIDTH else -1
-            return self.engine.viewer.get_screen_rgb_array(), reward, True, True, {}
-        return self.engine.viewer.get_screen_rgb_array(), 0.0, False, False, {}
-
-    def reset(self, options: dict, seed: Optional[int]=None, return_info: bool=False) -> Union[np.ndarray, Tuple[np.ndarray, dict]]:
-        if seed is not None: self.engine.seed(seed)
-        self.engine.reset(options['is_player2_serve'])
-        self._renderer.render_step()
-
-        screen = self.engine.viewer.get_screen_rgb_array() 
-        return (screen, {}) if return_info else screen
-
